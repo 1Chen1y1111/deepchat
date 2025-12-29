cbbd90be81dba02ac9c8795327e98ccbd044b6af
1Chen1y1111 <fun20001016@163.com>
2025-12-29 10:55:49 +0800

    feat: init shadcn

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   main: {},
@@ -11,6 +12,9 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
+    server: {
+      host: '0.0.0.0' // 防止代理干扰，导致 vite-electron 之间 ws://localhost:5713 和 http://localhost:5713 通信失败、页面组件无法加载
+    },
+    plugins: [tailwindcss(), vue()]
   }
 })
